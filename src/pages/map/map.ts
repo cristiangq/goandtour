6b4db2ede13131cfc3b833f63070acfd84abd2eb
Cristian Quintana <cristiangq@gmail.com>
2017-11-28 03:42:22 -0300

    Add Map

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+declare var google;
 
 @IonicPage()
 @Component({
@@ -8,8 +10,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MapPage {
 
+    @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 
   }
 
+  ionViewDidLoad(){
+    this.initMap();
+  }
+
+  initMap() {
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 14,
+      center: {lat: -27.50, lng: -64.86}
+    });
+  }
 }
