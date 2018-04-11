0a2eb54160515a6264e634b3e41bfe3282510046
Cristian Quintana <cristiangq@gmail.com>
2018-04-11 10:13:04 -0300

    fixes

---
+++ builds/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm ~/go-and-tour/builds/LasTermasTour.apk
+rm ~/go-and-tour/builds/app-release-unsigned.apk
+cp ~/go-and-tour/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk  ~/go-and-tour/builds
+cd ~/go-and-tour/builds && jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.jks app-release-unsigned.apk cristiangq
+cd ~/Android/Sdk/build-tools/26.0.2/ && ./zipalign -v 4 ~/go-and-tour/builds/app-release-unsigned.apk ~/go-and-tour/builds/LasTermasTour.apk
